b72635cf7c9f2b0b36748aac92e28773103b392e
Nico Afonso <nicolas.afonso@se.com>
2024-11-28 08:27:24 +0100

    All scenarios are passed

--- calculator.backend/Program.cs
+++ calculator.backend/Program.cs
@@ -17,11 +17,9 @@ namespace calculator.backend
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
