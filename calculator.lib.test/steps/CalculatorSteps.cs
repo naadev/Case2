b5202d27919299e72a709112c947fc19759cc055
Nico Afonso <nicolas.afonso@se.com>
2024-11-28 07:56:12 +0100

    Basic skeleton of specification implementation. Also, first cucumber test

---
+++ calculator.lib.test/steps/CalculatorSteps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.CommonModels;
+
+namespace calculator.lib.test.steps
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int firstNumber)
+        {
+            _scenarioContext.Add("firstNumber", firstNumber);
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int secondNumber)
+        {
+            _scenarioContext.Add("secondNumber", secondNumber);
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            var firstNumber = _scenarioContext.Get<int>("firstNumber");
+            var secondNumber = _scenarioContext.Get<int>("secondNumber");
+            var result = Calculator.Add(firstNumber, secondNumber);
+            _scenarioContext.Add("result", result);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expectedResult)
+        {
+            var result = _scenarioContext.Get<int>("result");
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
