e1f6fe01408fd04c7e667fb886b2b193eae680f6
Nico Afonso <nicolas.afonso@se.com>
2024-11-28 08:27:24 +0100

    All scenarios are passed

--- calculator.backend/Controllers/CalculatorController.cs
+++ calculator.backend/Controllers/CalculatorController.cs
@@ -9,31 +9,31 @@ namespace CalculatorAPI.Controllers
         [HttpGet("add")]
         public ActionResult<double> Add([FromQuery] double a, [FromQuery] double b)
         {
-            return Ok(1);
+            return Ok(new { result = 1 });
         }
 
         [HttpGet("subtract")]
         public ActionResult<double> Subtract([FromQuery] double a, [FromQuery] double b)
         {
-            return Ok(2);
+            return Ok(new { result = 2 });
         }
 
         [HttpGet("multiply")]
         public ActionResult<double> Multiply([FromQuery] double a, [FromQuery] double b)
         {
-            return Ok(3);
+            return Ok(new { result = 3 });
         }
 
         [HttpGet("divide")]
         public ActionResult<double> Divide([FromQuery] double a, [FromQuery] double b)
         {
-            return Ok(4);
+            return Ok(new { result = 4 });
         }
 
         [HttpGet("is_prime")]
         public ActionResult<bool> IsPrime([FromQuery] int number)
         {
-            return Ok(true);
+            return Ok(new { result = true });
         }
     }
 }
