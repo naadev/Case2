39c6fa3fe411ce704431ee4103fcddf7c383f681
Nico Afonso <nicolas.afonso@se.com>
2024-11-29 07:37:39 +0100

    Created first test (failed) on mock

---
+++ calculator.backend/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculator.backend.Controllers
+{
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult Welcome()
+        {
+            return Ok(new { message = "welcome" });
+        }
+    }
+}
