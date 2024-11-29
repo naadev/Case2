046d4c31e85210800ebc0e3cf68a365be1ef8c14
Nico Afonso <nicolas.afonso@se.com>
2024-11-29 07:37:39 +0100

    Created first test (failed) on deployment solution.

---
+++ calculator.backend.test/steps/PrimeNumberSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace calculator.lib.test.steps
+{
+    [Binding]
+    public class PrimeNumberSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        public PrimeNumberSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [When(@"number (.*) is checked")]
+        public void WhenNumberIsChecked(int number)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"the answer to know whether is prime or not is No")]
+        public void ThenTheAnswerToKnowWhetherIsPrimeOrNotIsNo()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"the answer to know whether is prime or not is Yes")]
+        public void ThenTheAnswerToKnowWhetherIsPrimeOrNotIsYes()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
