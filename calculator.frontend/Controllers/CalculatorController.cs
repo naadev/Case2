e9cb82e08c4a55e58e4eae80d8c8df4fccfa1262
Nico Afonso <nicolas.afonso@se.com>
2024-12-01 14:43:10 +0100

    Add and "subtract" refactored

--- calculator.frontend/Controllers/CalculatorController.cs
+++ calculator.frontend/Controllers/CalculatorController.cs
@@ -11,58 +11,48 @@ namespace calculator.frontend.Controllers
         }
         const string base_url = "https://calculatorbackend-mock.azurewebsites.net";
         const string api = "api/Calculator";
+        private double ExecuteOperation(string operation, double num1, double num2)
+        {
+            var result = 0.0;
+            var clientHandler = new HttpClientHandler();
+            var client = new HttpClient(clientHandler);
+            var url = $"{base_url}/{api}/{operation}?a={num1}&b={num2}";
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+            };
+            using (var response = client.Send(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = response.Content.ReadAsStringAsync().Result;
+                var json = JObject.Parse(body);
+                var result_json = json["result"];
+                if (result_json != null)
+                {
+                    result = result_json.Value<double>();
+                }
+            }
+            return result;
+        }
         [HttpPost]
         public ActionResult Index(string firstNumber, string secondNumber, string operation)
         {
             double num1 = Convert.ToDouble(firstNumber);
             double num2 = Convert.ToDouble(secondNumber);
-            var clientHandler = new HttpClientHandler();
-            var client = new HttpClient(clientHandler);
             double result = 0;
             switch (operation)
             {
                 case "Add":
                     {
-                        operation = "Add";
-                        var url = $"{base_url}/{api}/{operation}?a={num1}&b={num2}";
-                        var request = new HttpRequestMessage
-                        {
-                            Method = HttpMethod.Get,
-                            RequestUri = new Uri(url),
-                        };
-                        using (var response = client.Send(request))
-                        {
-                            response.EnsureSuccessStatusCode();
-                            var body = response.Content.ReadAsStringAsync().Result;
-                            var json = JObject.Parse(body);
-                            var result_json = json["result"];
-                            if (result_json != null)
-                            {
-                                result = result_json.Value<double>();
-                            }
-                        }
+                        operation = "add";
+                        result = ExecuteOperation(operation, num1, num2);
                     }
                     break;
                 case "Subtract":
                     {
                         operation = "subtract";
-                        var url = $"{base_url}/{api}/{operation}?a={num1}&b={num2}";
-                        var request = new HttpRequestMessage
-                        {
-                            Method = HttpMethod.Get,
-                            RequestUri = new Uri(url),
-                        };
-                        using (var response = client.Send(request))
-                        {
-                            response.EnsureSuccessStatusCode();
-                            var body = response.Content.ReadAsStringAsync().Result;
-                            var json = JObject.Parse(body);
-                            var result_json = json["result"];
-                            if (result_json != null)
-                            {
-                                result = result_json.Value<double>();
-                            }
-                        }
+                        result = ExecuteOperation(operation, num1, num2);
                     }
                     break;
                 case "Multiply":
