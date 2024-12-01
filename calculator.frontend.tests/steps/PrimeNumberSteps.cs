53752afb0cba26d7f5bf9ed74ad6f67ed1f87c3e
Nico Afonso <nicolas.afonso@se.com>
2024-12-01 12:23:36 +0100

    Skeleton of MVC solution is completed.
    Also, tests for basic operations are added.
    
    Pending to add PrimeNumber tests.

---
+++ calculator.frontend.tests/steps/PrimeNumberSteps.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace calculator.frontend.tests.steps
+{
+    [Binding]
+    public class PrimeNumberSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public PrimeNumberSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [When(@"number (.*) is checked")]
+        public async Task WhenNumberIsChecked(int number)
+        {
+            IPage page = _scenarioContext.Get<IPage>("page");
+            await page.GotoAsync("https://calculator-frontend-master-ugr.azurewebsites.net/Attribute");
+            await page.FillAsync("#number", number.ToString());
+            await page.ClickAsync("#attribute");
+        }
+
+        [Then(@"the answer to know whether is prime or not is (.*)")]
+        public async Task ThenTheAnswerToKnowWhetherIsPrimeOrNotIs(string isPrime)
+        {
+            var page = (IPage)_scenarioContext["page"];
+            var resultText = await page.InnerTextAsync("#isPrime");
+            Assert.Equal(isPrime, resultText);
+        }
+    }
+}
---
+++ calculator.frontend/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculator.frontend.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string firstNumber, string secondNumber, string operation)
+        {
+            double num1 = Convert.ToDouble(firstNumber);
+            double num2 = Convert.ToDouble(secondNumber);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "Add":
+                    result = 0;
+                    break;
+                case "Subtract":
+                    result = 0;
+                    break;
+                case "Multiply":
+                    result = 0;
+                    break;
+                case "Divide":
+                    result = 0;
+                    break;
+            }
+
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
